4f09a7bbf84f0726dff6f63f06fb2235ceae9716
Safaa Soliman <safaasoliman1991@gmail.com>
2025-10-24 18:56:39 +0400

    Adding the BuildingBlocks class library

---
+++ src/CreateProductEndpoint.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class CreateProductEndpoint
+{
+	public CreateProductEndpoint()
+	{
+	}
+}
---
+++ src/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public record CreateProductCommand(
+	string Name, List<string> Category,
+	string Description, string ImageFile,
+	decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid id);
+
+
+internal class CreateProductCommandHandler
+	:IRequestHandler<CreateProductCommand,CreateProductResult>
+{
+	public Task<CreateProductResult> Handle(CreateProductCommand request,CancellationToken cancellationToken)
+}
